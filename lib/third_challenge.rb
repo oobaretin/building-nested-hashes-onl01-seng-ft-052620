16279eb5e7b2e7558c0668b6f8174dec01d17b4a
osagie obaretin <osagie.codes@gmail.com>
2020-06-06 01:32:19 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,42 +3,36 @@ def third_challenge
    :montague => {
       :patriarch => {
         :name => "Lord Montague",
-      :age => "53"
+        :age => "53"
         },
       :matriarch => {
         :name => "Lady Montague",
-      :age => "54"
+        :age => "54"
         },
       :hero => {
         :name => "Romeo",
-      :age => "15"
-      :status => "alive"
+        :age => "15",
+        :status => "alive"
         },
-      :hero_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Mercutio", age: "18", attitude: "hot-headed"}
-        ]
-   }, 
+      :hero_friends => []
+   },
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet",
-      :age => "50"
+        :age => "50"
         },
       :matriarch => {
         :name => "Lady Capulet",
-      :age => "51"
+        :age => "51"
         },
       :heroine => {
-      :name => "Juliet",
-      :age => "15",
-      :status => "alive"
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
-      :heroine_friends => [
-        {name: "Steven", age: "30", attitude: "confused"},
-          {name: "Nurse", age: "44", attitude: "worried"}
-        ]
+      :heroine_friends => []
    }
   }
 
-  
+
 end

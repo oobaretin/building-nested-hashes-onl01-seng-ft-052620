098b3e22fad2bd8bc013b49f206882b3b59ff56c
osagie obaretin <osagie.codes@gmail.com>
2020-06-06 01:24:38 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,8 @@ def third_challenge
       :age => "15"
         },
       :hero_friends => [
-        
+        {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}
         ]
    }, 
    :capulet => {
